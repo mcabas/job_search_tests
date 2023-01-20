0adec3f3e8099df1f8f98bd42aac26b36d65c191
Michael Cabas <mcabas.ingbio@gmail.com>
2023-01-19 23:41:07 -0500

    familia

---
+++ Familia test/query1.sql
@@ -0,0 +1,40 @@
+--create table cos2017
+CREATE TABLE cos2017
+(rank integer,
+name varchar,
+country_of_origin varchar,
+retail_revenue NUMERIC,
+parent_company_revenue NUMERIC,
+parent_company_net_income NUMERIC,
+dominant_operational_format varchar,
+countries_of_operation integer,
+retail_revenue_cagr NUMERIC
+);
+
+--create table cos2018
+CREATE TABLE cos2018
+(rank integer,
+name varchar,
+country_of_origin varchar,
+retail_revenue NUMERIC,
+parent_company_revenue NUMERIC,
+parent_company_net_income NUMERIC,
+dominant_operational_format varchar,
+countries_of_operation integer,
+retail_revenue_cagr NUMERIC
+);
+
+--create table cos2019
+CREATE TABLE cos2019
+(rank integer,
+name varchar,
+country_of_origin varchar,
+retail_revenue NUMERIC,
+parent_company_revenue NUMERIC,
+parent_company_net_income NUMERIC,
+dominant_operational_format varchar,
+countries_of_operation integer,
+retail_revenue_cagr NUMERIC
+);
+
+
