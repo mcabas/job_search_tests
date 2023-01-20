3f3efcf4e932958f46111e81d997ac22db860f14
Michael Cabas <mcabas.ingbio@gmail.com>
2023-01-20 09:21:12 -0500

    familia

--- Familia test/query1.sql
+++ Familia test/query1.sql
@@ -37,4 +37,62 @@ countries_of_operation integer,
 retail_revenue_cagr NUMERIC
 );
 
+--Which are the retailers that got to be included in the TOP 10 for 2017, 2018 and 2019?
+
+--cos2017
+select name, retail_revenue
+from cos2017
+order by retail_revenue desc
+limit 10
+--cos2018
+select name, retail_revenue
+from cos2018
+order by retail_revenue desc
+limit 10
+--cos2019
+select name, retail_revenue
+from cos2019
+order by retail_revenue desc
+limit 10
+
+--Get a list of the retailers that are included in the TOP 20 in 2017 and 2019, broken down in alphabetical order. 
+
+SELECT
+  name, retail_revenue
+from (SELECT name, retail_revenue FROM cos2017
+	UNION ALL
+	SELECT name, retail_revenue FROM cos2019
+  	order by retail_revenue desc, name asc
+	limit 20) AS cos2017_2019
+order by name asc
+limit 20
+
+--List the top 15 retailers with the highest retail revenue cagr in 2018
+
+select name, retail_revenue_cagr
+from cos2018
+WHERE retail_revenue_cagr IS NOT NULL
+order by retail_revenue_cagr desc
+limit 15
+
+--What is the average retail revenue of retailers in 2017?
+select AVG(retail_revenue)
+from cos2017
+
+--Get the top 5 retailers by market share of retail revenue for retailer with presence in at least 10 countries.
+
+SELECT
+  name, retail_revenue, countries_of_operation
+from (SELECT name, retail_revenue, countries_of_operation FROM cos2017
+	UNION
+	SELECT name, retail_revenue, countries_of_operation FROM cos2018
+	UNION
+	SELECT name, retail_revenue, countries_of_operation FROM cos2019
+  	order by retail_revenue desc
+	) AS cos2017_2018_2019
+where countries_of_operation >= 10
+order by retail_revenue desc
+limit 5
+
+--
 
