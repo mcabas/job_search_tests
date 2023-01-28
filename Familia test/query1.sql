2de00e65c54fd9254ef5b95b25aebf485bc28e38
Michael Cabas <mcabas.ingbio@gmail.com>
2023-01-27 21:52:37 -0500

    Familia

--- Familia test/query1.sql
+++ Familia test/query1.sql
@@ -1,3 +1,12 @@
+--SQL Test
+--You were given a data set containing the ranking of the top 250 global retailers. 
+--You are asked to answer the following questions both in SQL and Python. 
+
+--For SQL: write the queries that you used to answer each question and add 
+--an image of the results. 
+
+--1. Describe the steps you took to have data ready to complete the test.
+
 --create table cos2017
 CREATE TABLE cos2017
 (rank integer,
@@ -37,7 +46,12 @@ countries_of_operation integer,
 retail_revenue_cagr NUMERIC
 );
 
---Which are the retailers that got to be included in the TOP 10 for 2017, 2018 and 2019?
+--Step 4. Refresh Database
+--Step 5. Go to the cos2017 table and click to open import/export, in options select import, and look for the cos2017.csv file in downloads, update header as ‘yes’ and delimiter as ‘;’, then go to the columns tab and validate that the 9 columns are selected.
+--Step 6. Click on OK.
+--Step 7. Repeat steps 4-6 for the cos2018.csv  and cos2019.csv files.
+
+--2. Which are the retailers that got to be included in the TOP 10 for 2017, 2018 and 2019?
 
 --cos2017
 select name, retail_revenue
@@ -55,7 +69,7 @@ from cos2019
 order by retail_revenue desc
 limit 10
 
---Get a list of the retailers that are included in the TOP 20 in 2017 and 2019, broken down in alphabetical order. 
+--3. Get a list of the retailers that are included in the TOP 20 in 2017 and 2019, broken down in alphabetical order. 
 
 SELECT
   name, retail_revenue
@@ -67,7 +81,7 @@ from (SELECT name, retail_revenue FROM cos2017
 order by name asc
 limit 20
 
---List the top 15 retailers with the highest retail revenue cagr in 2018
+--4. List the top 15 retailers with the highest retail revenue cagr in 2018
 
 select name, retail_revenue_cagr
 from cos2018
@@ -75,11 +89,11 @@ WHERE retail_revenue_cagr IS NOT NULL
 order by retail_revenue_cagr desc
 limit 15
 
---What is the average retail revenue of retailers in 2017?
+--5. What is the average retail revenue of retailers in 2017?
 select AVG(retail_revenue)
 from cos2017
 
---Get the top 5 retailers by market share of retail revenue for retailer with presence in at least 10 countries.
+--6. Get the top 5 retailers by market share of retail revenue for retailer with presence in at least 10 countries.
 
 SELECT
   name, retail_revenue, countries_of_operation
@@ -94,5 +108,22 @@ where countries_of_operation >= 10
 order by retail_revenue desc
 limit 5
 
---
+--7. Which is the % of retailers in the ranking of 2019 that sell in 
+--	more than 5 countries?
 
+WITH total_retailers AS (
+    SELECT COUNT(*) as total
+    FROM cos2019
+)
+SELECT (SUM(CASE WHEN countries_of_operation > 5 THEN 1 ELSE 0 END) / total.total)*100 as percentage
+FROM cos2019, total_retailers
+
+--8. Who is the retailer present in more countries in 2019 that also 
+--	has a revenue higher than the average revenue of all retailers in 2019?
+--9. How many retailers have a retail revenue cagr higher than 5%, 
+-- 	list them grouping them per year and in descendent order.
+--10. Identify records with nulls in any variable.
+--11. Create a variable to segment retailers by their retail revenue cagr:
+-- a.	<0,03 alias A
+-- b.	0,031-0,069 alias B
+-- c.	>0,07 alias C
